863f97eeb21b72f2b8e212e7a9d08a639550247c
Your Name <giwrgoshoulis@gmail.com>
2018-02-09 14:27:15 +0200

    αποθήκευση της μισης εργασιας

--- askhsh4.py
+++ askhsh4.py
@@ -1,2 +1,31 @@
 
-number= input ("Πληκτρολογήστε τον αριθμό που θέλετε να μετατρέψετε:")
+number= input ("Πληκτρολογήστε τον αριθμό που θέλετε να μετατρέψετε (μεχρι 1000000):")
+str(number)
+def reversed_string(number):
+    return number[::-1]
+mystring1 = "K"
+l = (reversed_string(number)[0])
+print (l)
+
+if l == str(1):
+	print ("hello");
+	mystring1 = "L";
+	
+
+
+
+
+
+
+
+
+
+mystring2 = "L"
+print (reversed_string(number)[0])
+print (mystring1,end="")
+print (mystring1,end="")
+print (mystring1,end="")
+print (mystring1,end="")
+print (mystring1,end="")
+print (mystring1,end="")
+print (mystring1,end="")
