467d2d9bcfff8598a9065f83e95a018d30ed8c2d
Your Name <giwrgoshoulis@gmail.com>
2018-02-20 18:15:32 +0200

    αρχη ασκησησης 2

---
+++ askhsh2.py
@@ -0,0 +1,55 @@
+import urllib.request, urllib.error, urllib.parse
+import json
+import datetime
+
+cur_date=datetime.datetime.now()
+a = [int(x) for x in input("Εισάγετε τους 10 αριθμούς σας χωρισμένους με κόμμα(,): ").split(',')]
+number1 = a[0]
+number2 = a[1]
+number3 = a[2]
+number4 = a[3]
+number5 = a[4]
+number6 = a[5]
+number7 = a[6]
+number8 = a[7]
+number9 = a[8]
+number10 = a[9]
+
+def compare_lists(l1,l2):
+    if 
+    s=0
+    for i in l1:
+        if i in l2:
+            s+=1
+	
+    	
+    return s
+mynums = []	
+mynums.append(number1)
+mynums.append(number2)
+mynums.append(number3)
+mynums.append(number4)
+mynums.append(number5)
+mynums.append(number6)
+mynums.append(number7)
+mynums.append(number8)
+mynums.append(number9)
+mynums.append(number10)
+
+for i in range(10):
+    cur_date= cur_date - datetime.timedelta(days=1)
+    date_str= cur_date.strftime("%d-%m-%Y")
+    url='http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json'%date_str
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data=json.loads(data)
+    klhrwseis= data['draws']['draw']
+    r=[]
+    for k in klhrwseis:
+        tmp=k["results"]
+        r.append(compare_lists(mynums,tmp))
+    print("apotelesmata",date_str)
+    print(min(r),r.count(min(r)))
+    print(max(r),r.count(max(r)))
+    print(10*"-")
