e9ec1d732b146e057f2d419e50535aa2120e9733
Your Name <giwrgoshoulis@gmail.com>
2018-02-21 18:02:10 +0200

    άσκηση2

--- askhsh2.py
+++ askhsh2.py
@@ -1,7 +1,6 @@
 import urllib.request, urllib.error, urllib.parse
 import json
 import datetime
-
 cur_date=datetime.datetime.now()
 a = [int(x) for x in input("Εισάγετε τους 10 αριθμούς σας χωρισμένους με κόμμα(,): ").split(',')]
 number1 = a[0]
@@ -14,16 +13,20 @@ number7 = a[6]
 number8 = a[7]
 number9 = a[8]
 number10 = a[9]
+y = 0
+b = ""
+def compare_lists(l1,l2,y,b):
 
-def compare_lists(l1,l2):
-    if 
-    s=0
-    for i in l1:
-        if i in l2:
-            s+=1
-	
-    	
-    return s
+	s=0
+	for i in l1:
+		if i in l2:
+			s+=1
+			if y < s + 1:		
+				y = s
+				b = (date_str)			
+	print (b)
+	return (y,s, b)
+
 mynums = []	
 mynums.append(number1)
 mynums.append(number2)
@@ -36,20 +39,18 @@ mynums.append(number8)
 mynums.append(number9)
 mynums.append(number10)
 
-for i in range(10):
-    cur_date= cur_date - datetime.timedelta(days=1)
-    date_str= cur_date.strftime("%d-%m-%Y")
-    url='http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json'%date_str
-    req = urllib.request.Request(url)
-    response = urllib.request.urlopen(req)
-    data = response.read()
-    data=json.loads(data)
-    klhrwseis= data['draws']['draw']
-    r=[]
-    for k in klhrwseis:
-        tmp=k["results"]
-        r.append(compare_lists(mynums,tmp))
-    print("apotelesmata",date_str)
-    print(min(r),r.count(min(r)))
-    print(max(r),r.count(max(r)))
-    print(10*"-")
+for i in range(31):
+	cur_date= cur_date - datetime.timedelta(days=1)
+	date_str= cur_date.strftime("%d-%m-%Y")
+	url='http://applications.opap.gr/DrawsRestServices/kino/drawDate/%s.json'%date_str
+	req = urllib.request.Request(url)
+	response = urllib.request.urlopen(req)
+	data = response.read()
+	data=json.loads(data)
+	klhrwseis= data['draws']['draw']
+	r=[]
+	for k in klhrwseis:
+		tmp=k["results"]
+		r.append(compare_lists(mynums,tmp,y,b))
+	
+print ("Η ημέρα στην οποία έχεται τις περισσότερες πιθανότητες νίκης στο Κίνο είναι η τελευταία ημερομηνία")
